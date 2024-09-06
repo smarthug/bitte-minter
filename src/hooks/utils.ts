b772db16fd32727452d6612d47e4b5a3b32889e1
smarthug <kirklayer@gmail.com>
2024-09-06 21:08:29 +0900

    minting works

--- src/hooks/utils.ts
+++ src/hooks/utils.ts
@@ -43,9 +43,9 @@ export const cbUrl = (hash: string, callbackArgs: CallbackArgs) =>
   callbackUrl(hash, TransactionSuccessEnum.MINT, callbackArgs);
 
 export const getTxnHash = async (hash: string) => {
-  const res = await fetch(`${nearblocksApi}/v1/search/?keyword=${hash}`);
+  // const res = await fetch(`${nearblocksApi}/v1/search/?keyword=${hash}`);
+  // const txn = await res.json();
+  // console.log(txn);
 
-  const txn = await res.json();
-
-  return txn?.receipts[0].originated_from_transaction_hash;
+  // return txn?.receipts[0].originated_from_transaction_hash;
 };
